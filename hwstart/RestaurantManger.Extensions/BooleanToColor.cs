d3e2d2c5a112a7301b13760a809b8df1d27bea35
LAM63 <LAM63@users.noreply.github.com>
2015-12-20 10:42:38 +0100

    Homework for module 3
    EDX Dev206.2X solution for Homework for module 3

---
+++ hwstart/RestaurantManger.Extensions/BooleanToColor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Windows.UI.Xaml.Data;
+using Windows.UI;
+
+namespace RestaurantManager.Extensions
+{
+    public class BooleanToColor : IValueConverter
+    {
+        public Color TrueColor { get; set; }
+        public Color FalseColor { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return (bool)value ? TrueColor : FalseColor;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            //Not needed by the application.
+            return (bool)(value = TrueColor) ? true : false;
+
+        }
+
+    }
+}
---
+++ hwstart/RestaurantManger.Extensions/PopupMessage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Microsoft.Xaml.Interactivity;
+using Windows.UI.Xaml.Input;
+
+namespace RestaurantManager.Extensions
+{
+    public class PopupMessage : DependencyObject, IBehavior
+    {
+        public string Message { get; set; }
+        public string Title { get; set; }
+
+        public DependencyObject AssociatedObject { get; private set; }
+
+        public void Attach(DependencyObject associatedObject)
+        {
+            if (associatedObject is Page)
+            {
+                this.AssociatedObject = associatedObject;
+                (this.AssociatedObject as Page).RightTapped += Page_RightTapped;
+            }
+        }
+
+        public void Detach()
+        {
+            if (this.AssociatedObject != null && this.AssociatedObject is Page)
+            {
+                (this.AssociatedObject as Page).RightTapped -= Page_RightTapped;
+            }
+        }
+
+        private async void Page_RightTapped(object sender, RightTappedRoutedEventArgs e)
+        {
+            await new MessageDialog(this.Message, this.Title).ShowAsync();
+        }
+    }
+}
